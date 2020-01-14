6635aa8524f40bff2c74e67b55bfdc786f293403
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-01-14 21:07:05 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+function sendColour (text: string) {
+    channel = 1
+    for (let index = 0; index < mbits; index++) {
+        radio.sendString(text)
+        channel += 1
+        radio.setGroup(channel)
+        basic.pause(delay)
+    }
+}
+input.onButtonPressed(Button.AB, function () {
+    sendColour("amber")
+})
+input.onButtonPressed(Button.B, function () {
+    sendColour("green")
+})
+input.onButtonPressed(Button.A, function () {
+    sendColour("red")
+})
+let delay = 0
+let mbits = 0
+let channel = 0
+channel = 1
+mbits = 12
+delay = 100
+basic.showNumber(channel)
+radio.setGroup(channel)
